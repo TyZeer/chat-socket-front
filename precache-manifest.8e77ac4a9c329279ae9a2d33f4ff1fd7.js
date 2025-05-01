605d4e0b50ceec82d837c82c7299167213cd8399
TyZeer <d.pavlov04@gmail.com>
2025-05-02 01:24:10 +0300

    Updates

---
+++ precache-manifest.8e77ac4a9c329279ae9a2d33f4ff1fd7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5adf75a440dbb38a83945e315e471604",
+    "url": "/chat-socket-front/index.html"
+  },
+  {
+    "revision": "13ee473d18fa23f6fc89",
+    "url": "/chat-socket-front/static/css/2.194f605c.chunk.css"
+  },
+  {
+    "revision": "c45afd8d306fd4ba20be",
+    "url": "/chat-socket-front/static/css/main.f2fd20fd.chunk.css"
+  },
+  {
+    "revision": "13ee473d18fa23f6fc89",
+    "url": "/chat-socket-front/static/js/2.5f97a39a.chunk.js"
+  },
+  {
+    "revision": "c706e8c584df1b58d40ce6b4ed4263f0",
+    "url": "/chat-socket-front/static/js/2.5f97a39a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c45afd8d306fd4ba20be",
+    "url": "/chat-socket-front/static/js/main.f0175a8a.chunk.js"
+  },
+  {
+    "revision": "e235a67bca6f67cac767",
+    "url": "/chat-socket-front/static/js/runtime-main.aed4b058.js"
+  },
+  {
+    "revision": "9c2591ac2eb8d637579bb7856ac1a6b0",
+    "url": "/chat-socket-front/static/media/notification.9c2591ac.mp3"
+  },
+  {
+    "revision": "1f0a44cdf04e09a8f12bd13812790e7a",
+    "url": "/chat-socket-front/static/media/user.1f0a44cd.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d9f696e0a6577faa6249e5b164f9acb0.js"
+  "/chat-socket-front/precache-manifest.8e77ac4a9c329279ae9a2d33f4ff1fd7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/chat-socket-front/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
